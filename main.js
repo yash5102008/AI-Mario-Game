817b64bb3fdbc2b218ecff68fcae7c5d87effebc
yash5102008 <75563395+yash5102008@users.noreply.github.com>
2022-08-03 15:36:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_die = loadSound("mariodie.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose' , gotPoses);
+
+	function modelLoaded() {
+		console.log('model Loaded');
+	}
+
+	function gotPoses(results)
+	{
+		if(results.length > 0)
+		{
+		  console.log("noseX = " + noseX +", noseY" + noseY);
+		  noseX = results[0].pose.nose.x;
+		  noseY = results[0].pose.nose.y;
+		}
+	}
+}
+
+function draw() {
+	game();
+}
+
+
+
+
+
+
